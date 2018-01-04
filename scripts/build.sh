f0c2c1a1e3e524616d437870c9e0e3c2ee83abe2
Reece Bradley <reece.io@outlook.com>
2018-01-04 05:34:37 -0600

    still troubleshooting travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+echo "ls"
 ls
 
+echo "gem query --local (gems installed)"
 gem query --local
 
-jekyll -v
+echo "bundle exec jekyll -v"
+bundle exec jekyll -v
 
-echo "jekyll build"
-jekyll build --config _config.yml
+echo "jekyll build --config _config.yml"
+bundle exec jekyll build --config _config.yml
 
-echo "htmlproofer"
+echo "htmlproofer ./_site --url-ignore www.youtube.com"
 htmlproofer ./_site --url-ignore www.youtube.com
