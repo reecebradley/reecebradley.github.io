7c4eea674df4de826e1d67aab0db71339da3fbac
Reece Bradley <reece.io@outlook.com>
2018-01-04 05:50:47 -0600

    fixed travis, trying alternate approach

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo -e "\x1B[31m -- ls \x1B[0m"
+echo -e "\x1B[31m **** $ ls \x1B[0m"
 ls
 
-echo "-- gem update"
-gem update
+#echo "-- gem update"
+#gem update
 
-echo "-- gem query --local (gems installed)"
+echo -e "\x1B[31m **** $ gem query --local (gems installed) \x1B[0m"
 gem query --local
 
-echo "-- bundle exec jekyll -v"
+echo -e "\x1B[31m **** $ bundle exec jekyll -v \x1B[0m"
 bundle exec jekyll -v
 
-echo "-- jekyll build --config _config.yml"
+echo -e "\x1B[31m **** $ jekyll build --config _config.yml \x1B[0m"
 bundle exec jekyll build --config _config.yml
 
-echo "-- htmlproofer ./_site --url-ignore www.youtube.com"
+echo -e "\x1B[31m **** $ htmlproofer ./_site --url-ignore www.youtube.com \x1B[0m"
 htmlproofer ./_site --url-ignore www.youtube.com
