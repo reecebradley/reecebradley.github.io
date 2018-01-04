55db4ffc12b615ecb6e0505efa5eae6f0695bddf
Reece Bradley <reece.io@outlook.com>
2018-01-04 05:06:05 -0600

    troubleshooting travis

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+ls
+
+gem query --local
+
+jekyll -v
+
+echo "jekyll build"
 jekyll build --config _config.yml
+
+echo "htmlproofer"
 htmlproofer ./_site --url-ignore www.youtube.com
